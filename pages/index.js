683e7299923033f4c8380ac8897289725c8b7941
sinoxis <89727501+sinoxis@users.noreply.github.com>
2023-10-06 19:24:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import copy from "copy-to-clipboard";
-import Head from "next/head";
 
 const Index = () => {
   const [videoURL, setVideoURL] = useState("");
@@ -11,7 +10,7 @@ const Index = () => {
     let match = url.match(regExp);
 
     if (match && match[1].length === 11) {
-      const videoId = match[1];
+      const videoURL = match[1];
       const thumbnailBaseUrl = "http://img.youtube.com/vi/";
 
       const options = [
@@ -24,7 +23,7 @@ const Index = () => {
 
       const thumbnailOptions = options.map((option) => ({
         resolution: option.resolution,
-        url: `${thumbnailBaseUrl}${videoId}/${option.code}.jpg`,
+        url: `${thumbnailBaseUrl}${videoURL}/${option.code}.jpg`,
       }));
 
       setThumbnailOptions(thumbnailOptions);
@@ -34,36 +33,47 @@ const Index = () => {
     }
   };
 
-  // Load Google Analytics when the component mounts
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-JJ2MNZKP7L";
-    script.async = true;
-    script.onload = () => {
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-      gtag("config", "G-JJ2MNZKP7L");
-    };
-    document.head.appendChild(script);
-  }, []);
-
   return (
     <div className="container mx-auto px-4 py-8">
-      <Head>
-        {/* Google Analytics script is loaded asynchronously */}
-      </Head>
       <header className="text-center mb-8">
-        {/* ... (Your header content) */}
+        <h1 className="text-3xl font-bold mb-2">
+          Youtube Thumbnail Downloader
+        </h1>
+        <p className="text-gray-600">
+          Download high-quality thumbnails from YouTube videos.
+        </p>
       </header>
       <div className="text-center">
-        {/* ... (Your input field and button) */}
+        <input
+          type="text"
+          className="w-full md:w-1/2 px-4 py-2 border rounded"
+          placeholder="Enter YouTube URL"
+          value={videoURL}
+          onChange={(e) => setVideoURL(e.target.value)}
+        />
+        <button
+          className="btn-blue mt-2"
+          onClick={() => getYouTubeThumbnail(videoURL)}
+        >
+          Download Thumbnails
+        </button>
       </div>
       {thumbnailOptions.length > 0 && (
         <div className="mt-8">
-          {/* ... (Thumbnail options and buttons) */}
+          <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {thumbnailOptions.map((option, index) => (
+              <div key={index} className="thumbnail-option">
+                <img src={option.url} alt={`Thumbnail ${index + 1}`} />
+                <button
+                  className="btn-blue mt-2"
+                  onClick={() => copy(option.url)}
+                >
+                  Copy Image URL
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
